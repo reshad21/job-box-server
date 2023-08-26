2dc6c724f70ad0bfd96c431e521dd3815c4ca6c5
reshad21 <miakala7@gmail.com>
2023-08-26 23:48:36 +0600

    process.env

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n5u1omq.mongodb.net/`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n5u1omq.mongodb.net/`;
+const uri = process.env.DB_URL;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
